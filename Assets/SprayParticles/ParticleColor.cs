ce9320beb15b4a558d33487430561a21c3b73fb7
geilepaste <nloewes@gmail.com>
2019-07-12 12:08:46 +0200

    Particle Effect
    With changeable colors

---
+++ Assets/SprayParticles/ParticleColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleColor : MonoBehaviour
+{
+    public ParticleSystem particles;
+    public ColorManager cm;
+
+    void changeColor()
+    {
+        particles.startColor = cm.color;
+    }
+}
