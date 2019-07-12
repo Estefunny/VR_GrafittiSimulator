207fe615c2ccb4645bb12ba16f46eb35dfecf67f
geilepaste <nloewes@gmail.com>
2019-07-12 13:43:40 +0200

    Boombox, Music Player, Particle Effect
    -Changed Sprayer.cs to also stop/activate particles
    -Added Boombox with Music Player

---
+++ Assets/Boombox/MusicPlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    public AudioSource player;
+    public AudioClip firstClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.clip = firstClip;
+        player.loop = false;
+
+    }
+
+    public void PlayPause()
+    {
+        if (player.isPlaying) {
+            player.Stop();
+        } else
+        {
+            player.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            PlayPause();
+        }
+    }
+}
