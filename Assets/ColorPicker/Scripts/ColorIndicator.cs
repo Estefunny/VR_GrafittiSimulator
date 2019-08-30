1398c0d7507bb444bedc97cc2f990ab91216e7e7
Esrion <ronreh7@gmail.com>
2019-08-30 15:53:02 +0200

    dfgdfgdfgdfg

--- Assets/ColorPicker/Scripts/ColorIndicator.cs
+++ Assets/ColorPicker/Scripts/ColorIndicator.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ColorIndicator : MonoBehaviour {
@@ -5,15 +6,45 @@ public class ColorIndicator : MonoBehaviour {
 	HSBColor color;
     public Renderer render;
 
+    private int savedColorMax = 8;
+    private LinkedList<Color> savedColorList = new LinkedList<Color>();
+    public Renderer[] savedColorRenderers;
+
+    public void saveColor(Color c)
+    {
+        savedColorList.AddFirst(c);
+        if (savedColorList.Count > savedColorMax)
+        {
+            savedColorList.RemoveLast();
+        }
+        updateSavedColors();
+    }
+
+    private void updateSavedColors()
+    {
+        int i = 0;
+        foreach (Color c in savedColorList)
+        {
+            //savedColorRenderers[i]
+            i++;
+        }
+    }
+
 	void Start() {
 		color = HSBColor.FromColor(render.sharedMaterial.GetColor("_Color"));
 		transform.parent.BroadcastMessage("SetColor", color);
         transform.parent.BroadcastMessage("OnColorChange", color, SendMessageOptions.DontRequireReceiver);
+
+        savedColorList.AddLast(Color.red);
+        savedColorList.AddLast(Color.green);
+        savedColorList.AddLast(Color.blue);
+        savedColorList.AddLast(Color.yellow);
+        updateSavedColors();
     }
 
 	void ApplyColor ()
 	{
-        render.sharedMaterial.SetColor ("_Color", color.ToColor());
+        render.sharedMaterial.SetColor("_Color", color.ToColor());
 		transform.parent.BroadcastMessage("OnColorChange", color, SendMessageOptions.DontRequireReceiver);
 	}
 
